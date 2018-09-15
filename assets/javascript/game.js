22b68dc943b4c8bb0cfc210f26127704370abb19
ohmez <kkid_92@hotmail.com>
2018-09-15 14:27:25 -0600

    found way to make clicks work outside doc.ready, learned I need to use classes more and not ID's so I have to change things and commiting in case it breaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,40 @@
 // Jquery used for everything required
 $(document).ready(function() {
-var ready = false; 
-    $(".click").on("click", function(event) {
-        var clicked = this;
-        $(clicked).attr("id","picked");
-        console.log(clicked.id);
-        $("#picked").appendTo("#chosen");
-       
-        // this doesn't work yet to simultaniously attach unclicked to enemies and clicked to chosen
-        setTimeout(function() {
-            var unclicked = [];
-            for (var x = 0; x < 4; x++) {
-                unclicked[x] = $("#photos");
-                console.log(unclicked);
-            $(unclicked[x]).attr("id","enemies");
-            $(unclicked[x]).prependTo("#enemyarea");
-        } // end for loop contained in timeoutdelay
-        },500);
-        //$("#photos").attr("class","enemies").delay(200);
-       console.log($("#photos"));
-        // notclicked.appendTo("#enemies");
-    });
-
-
-
-
-
-
-
-
-
+    
+    
 });
 // anything below this is outside the doc.ready 
+$(document).on("click", ".click", function (event) {
+    $(this).attr("id", "picked");
+    $("#picked").appendTo("#chosen");
+    console.log("start of function");
+
+    // this doesn't work yet to simultaniously attach unclicked to enemies and clicked to chosen
+    setTimeout(function () {
+        var unclicked = [];
+        for (var x = 0; x < 4; x++) {
+            unclicked[x] = $("#photos");
+            $(unclicked[x]).attr("id", "enemies");
+            $(unclicked[x]).prependTo("#enemyarea");
+        } // end for loop contained in timeoutdelay
+        console.log(unclicked);
+        $(".chose").css('display', 'none');      // this needs to be reset when game ends
+    }, 100);
+    // end of timeout contained in on click function
+    console.log("this function selected your character");
+    firstClick();   
+});
+// end on click function
+
+$(document).on("click", ".clicked", "#enemies", function (event) {
+    console.log(this);
+});
+                
+                
+                
+                
+                
+function firstClick () {
+$(".click").addClass("clicked");
+$(".click").removeClass("click");
+};
