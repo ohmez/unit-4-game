d204a95dc62938a7701f5bba3e1dddec0caf5bd7
ohmez <kkid_92@hotmail.com>
2018-09-15 16:19:46 -0600

    great strides on homework everything working great

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,25 @@
 // Jquery used for everything required
 $(document).ready(function() {
-    
+var heroR = false;
+var defenderR = false;   
     
 });
 // anything below this is outside the doc.ready 
 $(document).on("click", ".click", function (event) {
     $(this).attr("id", "picked");
+    $(this).addClass("hero");
     $("#picked").appendTo("#chosen");
     console.log("start of function");
-
+    
     // this doesn't work yet to simultaniously attach unclicked to enemies and clicked to chosen
     setTimeout(function () {
         var unclicked = [];
         for (var x = 0; x < 4; x++) {
             unclicked[x] = $("#photos");
             $(unclicked[x]).attr("id", "enemies");
-            $(unclicked[x]).prependTo("#enemyarea");
+            $(unclicked[x]).addClass("enemy"+x).addClass("enemies");
+            $(unclicked[x]).appendTo("#enemyarea");
         } // end for loop contained in timeoutdelay
-        console.log(unclicked);
         $(".chose").css('display', 'none');      // this needs to be reset when game ends
     }, 100);
     // end of timeout contained in on click function
@@ -26,15 +28,41 @@ $(document).on("click", ".click", function (event) {
 });
 // end on click function
 
-$(document).on("click", ".clicked", "#enemies", function (event) {
+$(document).on("click", ".hero", ".clicked", function (event) {
+    console.log(this.attributes[2].value);
+    console.log(this.firstElementChild.innerText +" has " +this.lastElementChild.innerText + " health.");
+    console.log($(".hero"));
+});
+$(document).on("click", ".enemies", ".clicked", function (event) {
+    console.log(this.firstElementChild.innerText +" has " + this.lastElementChild.innerText + " health.");
+    console.log(this);
+    $(this).addClass("defender").removeClass("enemies").appendTo(".defend");
+    $("#picked").appendTo(".fightsection");
+    setTimeout(function () {
+        $(".enemies").css('display', 'none');
+        $("p.chosen").css('display', 'none');
+    },50);
+    // end timeout function
+    defense();
+    
+});
+$(document).on("click", ".defender", ".clicked", function (event) {
+    console.log(this.firstElementChild.innerHTML +" has " + this.lastElementChild.innerHTML + " health.");
+    
+});
+$(document).on("click", "#attack", function (event) {
+    if (heroR && defenderR) {
+        console.log("hero and defender is true");
+    }
     console.log(this);
 });
                 
-                
-                
-                
+function defense () {
+    defenderR = true;
+};              
                 
 function firstClick () {
 $(".click").addClass("clicked");
 $(".click").removeClass("click");
+    heroR = true;
 };
